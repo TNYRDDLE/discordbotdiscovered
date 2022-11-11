01c570711124e641eb1fcf8f008136e04048b31b
TNYRDDLE <105783214+TNYRDDLE@users.noreply.github.com>
2022-11-11 13:01:49 +0100

    Init
    init

---
+++ main.js
@@ -0,0 +1,56 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+
+const client = new Client({
+    intents: [
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildBans,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+    ],
+   
+  });
+
+const prefix = '!';
+
+
+client.once('ready', () => { 
+    console.log('Dee is online!');
+});
+
+client.on('messageCreate', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'ping'){
+        message.channel.send('pong!')
+    } else if(command === 'hello'){
+        message.channel.send('hello world')
+    }
+});
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.token);
+
